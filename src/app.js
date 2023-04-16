fa697a1a3afd0181213e5723d4c29516f942caf5
SolForte <mateusbrandao1234@gmail.com>
2023-04-16 02:59:41 -0300

    Mongodb setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
 
 // Criação do servidor
 const app = express();
@@ -7,6 +8,14 @@ const app = express();
 // Configurações
 app.use(express.json());
 app.use(cors());
+
+// Setup do Banco de Dados
+let db;
+const mongoClient = new MongoClient("mongodb://localhost:27017/nomeDoBanco");
+mongoClient
+  .connect()
+  .then(() => (db = mongoClient.db()))
+  .catch((err) => console.log(err.message));
 
 // Deixa o app escutando, à espera de requisições
 const PORT = 5000;
