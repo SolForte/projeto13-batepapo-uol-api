770b4129fd0a7dac633b0b18b42dca82f8b7ce83
SolForte <mateusbrandao1234@gmail.com>
2023-04-16 03:08:03 -0300

    Blank requisites

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 // Criação do servidor
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 // Configurações
 app.use(express.json());
 app.use(cors());
-dotenv.config()
+dotenv.config();
 
 // Setup do Banco de Dados
 let db;
@@ -19,6 +19,19 @@ mongoClient
   .then(() => (db = mongoClient.db()))
   .catch((err) => console.log(err.message));
 
-// Deixa o app escutando, à espera de requisições
+const PARTICIPANTE_TEMPLATE = {name: 'João', lastStatus: 12313123};
+const MESSAGE_TEMPLATE = {from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'};
+
+app.post("/participants", async (req, res) => {})
+
+app.get("/participants", async (req, res) => {})
+
+app.post("/messages", async (req, res) => {})
+
+app.get("/messages", async (req, res) => {})
+
+app.post("/status", async (req, res) => {})
+
+  // Deixa o app escutando, à espera de requisições
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
