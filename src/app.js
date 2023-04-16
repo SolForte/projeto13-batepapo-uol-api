d14c9bfd9fb0b9d7a835e1a1a1cc348bc2c9914f
SolForte <mateusbrandao1234@gmail.com>
2023-04-16 07:53:55 -0300

    Return code smells

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import dayjs from "dayjs";
 
 const INTERVAL_TIMER = 15000;
 const IMPERMANENCE_THRESHOLD = 10000;
-const TRUE = 1;
+const MINIMUN_LENGTH = 1;
 
 const app = express();
 
@@ -23,15 +23,15 @@ mongoClient
   .catch((erro) => console.log(erro.message));
 
 const participantSchema = joi.object({
-  name: joi.string().min(TRUE).required(),
+  name: joi.string().min(MINIMUN_LENGTH).required(),
 });
 const messageSchema = joi.object({
-  to: joi.string().min(TRUE).required(),
-  text: joi.string().min(TRUE).required(),
+  to: joi.string().min(MINIMUN_LENGTH).required(),
+  text: joi.string().min(MINIMUN_LENGTH).required(),
   type: joi.string().valid("message", "private_message").required(),
 });
 const limitSchema = joi.object({
-  limit: joi.number().integer().min(TRUE),
+  limit: joi.number().integer().positive(),
 });
 
 app.post("/participants", async (req, res) => {
@@ -43,7 +43,8 @@ app.post("/participants", async (req, res) => {
 
   if (validation.error) {
     const errors = validation.error.details.map((detail) => detail.message);
-    return res.status(422).send(errors);
+    res.status(422).send(errors);
+    return;
   }
 
   try {
@@ -83,8 +84,10 @@ app.get("/participants", async (req, res) => {
   try {
     const participants = await db.collection("participants").find().toArray();
     res.status(200).send(participants);
+    return;
   } catch (error) {
     res.status(500).send(error.message);
+    return;
   }
 });
 
@@ -98,7 +101,8 @@ app.post("/messages", async (req, res) => {
 
   if (validation.error) {
     const errors = validation.error.details.map((detail) => detail.message);
-    return res.status(422).send(errors);
+    res.status(422).send(errors);
+    return;
   }
 
   try {
@@ -120,8 +124,10 @@ app.post("/messages", async (req, res) => {
     });
 
     res.sendStatus(201);
+    return;
   } catch (error) {
     res.status(500).send(error.message);
+    return;
   }
 });
 
@@ -136,7 +142,6 @@ app.get("/messages", async (req, res) => {
         $or: [{ to: "Todos" }, { to: user }, { from: user }],
       })
       .toArray();
-
     if (limit) {
       const validation = limitSchema.validate(
         { limit },
@@ -144,7 +149,6 @@ app.get("/messages", async (req, res) => {
           abortEarly: false,
         }
       );
-
       if (validation.error) {
         const errors = validation.error.details.map((detail) => detail.message);
         res.status(422).send(errors);
@@ -152,8 +156,10 @@ app.get("/messages", async (req, res) => {
       }
     }
     res.status(200).send(messages.slice(-limit));
+    return;
   } catch (error) {
     res.status(500).send(error.message);
+    return;
   }
 });
 
@@ -162,6 +168,7 @@ app.post("/status", async (req, res) => {
 
   if (!user) {
     res.sendStatus(404);
+    return;
   }
 
   try {
@@ -179,8 +186,10 @@ app.post("/status", async (req, res) => {
     });
 
     res.sendStatus(200);
+    return;
   } catch (error) {
     res.status(500).send(error.message);
+    return;
   }
 });
 
