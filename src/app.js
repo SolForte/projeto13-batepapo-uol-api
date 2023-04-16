8c404b88e862acb1b90254cd9c734e03141a1e72
SolForte <mateusbrandao1234@gmail.com>
2023-04-16 04:56:13 -0300

    Route: POST /messages

--- src/app.js
+++ src/app.js
@@ -13,30 +13,36 @@ app.use(cors());
 dotenv.config();
 
 // Setup do Banco de Dados
-let db;
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
 mongoClient
   .connect()
   .then(() => (db = mongoClient.db()))
-  .catch((err) => console.log(err.message));
+  .catch((erro) => console.log(erro.message));
 
-// Padrão para validação
-const MINIMUM_NAME_SIZE = 1;
-const participantsSchema = joi.object({
-  name: joi.string().min(MINIMUM_NAME_SIZE).required(),
+// Padrões para validação
+const MINIMUM_LENGTH = 1;
+
+const participantSchema = joi.object({
+  name: joi.string().min(MINIMUM_LENGTH).required(),
+});
+const messageSchema = joi.object({
+  to: joi.string().min(MINIMUM_LENGTH).required(),
+  text: joi.string().min(MINIMUM_LENGTH).required(),
+  type: joi.string().valid("message", "private_message").required(),
 });
 
 // Route: POST "/participants"
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
 
-  const name_validation = participantsSchema.validate({name}, {
+  const validation = participantSchema.validate(req.body, {
     abortEarly: false,
   });
 
-  if (name_validation.error) {
-    res.sendStatus(422);
-    return;
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
   }
 
   try {
@@ -59,11 +65,10 @@ app.post("/participants", async (req, res) => {
       to: "Todos",
       text: "entra na sala...",
       type: "status",
-      time: dayjs().format("HH:mm:ss")
+      time: dayjs().format("HH:mm:ss"),
     });
 
     res.sendStatus(201);
-
   } catch (error) {
     res.sendStatus(500).send(error.message);
   }
@@ -81,14 +86,40 @@ app.get("/participants", async (req, res) => {
 
 // Route: POST "/messages"
 app.post("/messages", async (req, res) => {
-    const {to, text, type} = req.body
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
 
-    const from = req.headers.user
+  const validation = messageSchema.validate(req.body, {
+    abortEarly: false,
+  });
 
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
 
+  try {
+    const name_existance = await db
+      .collection("participants")
+      .findOne({ name: user });
 
+    if (!name_existance) {
+      res.sendStatus(422);
+      return;
+    }
 
+    await db.collection("messages").insertOne({
+      to,
+      from: user,
+      text,
+      type,
+      time: dayjs().format("HH:mm:ss"),
+    });
 
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 // Route: GET "/messages"
